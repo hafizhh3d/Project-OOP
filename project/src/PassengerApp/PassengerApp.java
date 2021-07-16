92ffd15f446d9b2b82426388571641e7668b8256
Hafizh H <hafizh.h3d@gmail.com>
2021-07-17 06:33:07 +0800

    Merge pull request #3 from hafizhh3d/implement-inheritance-and-overriding
    implement inheritance & override

--- project/src/PassengerApp/PassengerApp.java
+++ project/src/PassengerApp/PassengerApp.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import Model.Passenger;
 import Model.Destination;
 import Model.Baggage;
+import Model.BusinessClass;
+import Model.EconomyClass;
+import Model.FirstClass;
 import java.util.Scanner;
 
 
@@ -18,20 +21,24 @@ import java.util.Scanner;
  */
 public class PassengerApp 
 {
-    static ArrayList<Passenger> passengerList = new ArrayList<>();
+    //static ArrayList<Passenger> passengerList = new ArrayList<>(); // Comment this line if You are going to use IDE Output
     
-    public static void addPassenger(Passenger p)
+    // Comment this function if You are going to use IDE Output
+    /*public static void addPassenger(Passenger p)
     {
         passengerList.add(p);
         //System.out.println("New Passenger Added");
     }
     
+    // Comment this function if You are going to use IDE Output
     public static void main(String[] args)
     {
         UI ui = new UI();
         ui.setVisible(true);
-    }        
-    /*public static void displayMenu() 
+    }        */
+    
+    // Comment this function if You are going to use User Interface Output
+    public static void displayMenu() 
     {
         System.out.println("------------------------------------------");
         System.out.println("\tCheck-In Baggage System");
@@ -42,15 +49,20 @@ public class PassengerApp
         System.out.print("Your choice: ");
     }
 
+    // Comment this function if You are going to use User Interface Output
     public static void main(String[] args)
     {
-        ArrayList<Passenger> passengerList = new ArrayList<>();
+        ArrayList<FirstClass> firstPassengerList = new ArrayList<>();
+        ArrayList<BusinessClass> businessPassengerList = new ArrayList<>();
+        ArrayList<EconomyClass> economyPassengerList = new ArrayList<>();
         
-        Passenger p;
+        FirstClass fc;
+        BusinessClass bc;
+        EconomyClass ec;
         Destination d;
         Baggage b;
         
-        String fn, ln, ci, co, s;
+        String fn, ln, ci, co, s, o;
         int num;
         double w;
         
@@ -78,34 +90,94 @@ public class PassengerApp
                     
                     System.out.print("Seat [F-First, B-Business, E-Economy]: "); s = in.nextLine();
                     
-                    d = new Destination(ci, co);
-                    p = new Passenger(fn, ln, s, d);
-                    
-                    System.out.print("Number of baggage: "); num = in.nextInt();
-                    for(int i = 0; i < num; i++)
+                    switch(s)
                     {
-                        System.out.print("\tBag " + (i+1) + " (in kg): "); w = in.nextDouble();
-                        b = new Baggage(w);
-                        p.checkIn(b);
+                        case "F":
+                            d = new Destination(ci, co);
+                            fc = new FirstClass(fn, ln, d);
+                            
+                            System.out.print("Number of baggage: "); num = in.nextInt();
+                            for(int i = 0; i < num; i++)
+                            {
+                                System.out.print("\tBag " + (i+1) + " (in kg): "); w = in.nextDouble();
+                                b = new Baggage(w);
+                                fc.checkIn(b);
+                            }
+                            
+                            firstPassengerList.add(fc);
+                            break;
+                        case "B":
+                            d = new Destination(ci, co);
+                            bc = new BusinessClass(fn, ln, d);
+                            
+                            System.out.print("Number of baggage: "); num = in.nextInt();
+                            for(int i = 0; i < num; i++)
+                            {
+                                System.out.print("\tBag " + (i+1) + " (in kg): "); w = in.nextDouble();
+                                b = new Baggage(w);
+                                bc.checkIn(b);
+                            }
+                            
+                            businessPassengerList.add(bc);
+                            break;
+                        case "E":
+                            d = new Destination(ci, co);
+                            ec = new EconomyClass(fn, ln, d);
+                            
+                            System.out.print("Number of baggage: "); num = in.nextInt();
+                            for(int i = 0; i < num; i++)
+                            {
+                                System.out.print("\tBag " + (i+1) + " (in kg): "); w = in.nextDouble();
+                                b = new Baggage(w);
+                                ec.checkIn(b);
+                            }
+                            
+                            economyPassengerList.add(ec);
+                            break;
                     }
-                   
-                    passengerList.add(p);
-                    
                     break;
                 case 2:
                     System.out.println("\n+++++ List of Passengers +++++");
                     
-                    for(int i = 0; i < passengerList.size(); i++)
-                    {
-                        System.out.println("\nPassenger #" + (i+1));
-                        passengerList.get(i).display();
-                    }
+                    System.out.print("\nType of Passenger to Display [F-First, B-Business, E-Economy]: "); s = in.nextLine();
                     
-                    System.out.println("\n");
-                    break; 
+                    switch(s)
+                    {
+                        case "F":
+                            System.out.println("\n+-+-+ List of Passengers +-+-+");
+                            
+                            for(int i = 0; i < firstPassengerList.size(); i++)
+                            {
+                                System.out.println("\nPassenger #" + (i+1));
+                                o = firstPassengerList.get(i).toString();
+                                System.out.println(o);
+                            }
+                            break;
+                        case "B":
+                            System.out.println("\n+-+-+ List of Passengers +-+-+");
+                            
+                            for(int i = 0; i < businessPassengerList.size(); i++)
+                            {
+                                System.out.println("\nPassenger #" + (i+1));
+                                o = businessPassengerList.get(i).toString();
+                                System.out.println(o);
+                            }
+                            break;
+                        case "E":
+                            System.out.println("\n+-+-+ List of Passengers +-+-+");
+                            
+                            for(int i = 0; i < economyPassengerList.size(); i++)
+                            {
+                                System.out.println("\nPassenger #" + (i+1));
+                                o = economyPassengerList.get(i).toString();
+                                System.out.println(o);
+                            }
+                            break;
+                    }
+                    break;
             }
         } while(choice != 3);
         
         System.out.println("Thank you for using this system :)");
-    }*/
+    }
 }
